5a1ca04955457242e86153e8eee782b695f948a0
Caleb Elliott <caleb123elliott@gmail.com>
2023-11-26 11:27:22 -0800

    add resume

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import { Col, Row } from "react-bootstrap"
 import './experience.css'
+import CustomLinkText from "../CustomLinkText/CustomLinkText"
 
 const experiences = [
   {
@@ -8,16 +9,16 @@ const experiences = [
     location: 'Anchor Operating System',
     title: 'Software Engineer',
     skills: ['React', 'Node.js', 'Serverless', 'GraphQL', 'Redux', 'Material-UI', 'DynamoDB'],
-    description: `Daily work on Anchor, a large scale software platform that provides a wide array of services related to ticketing. Services provided include: payment processing, ticketing, report generation, integration with 3rd party ticketing providers, helicopter flight scheduling, automated email and text services, and others.`
+    description: `Worked on a variety of features for the ticketing platform which powers famous attractions such as the Statue of Liberty, Alcatraz Island, Niagara Falls, and many others. Services rendered include payment processing, report generation, integration with third party ticketing providers, and more.`
   },
   {
     date: 'May 2022 - Aug 2022',
     location: 'CodeWorks',
     title: 'Full Stack Developer Student',
     skills: ['Vue', 'Node.js', 'CSS', 'Bootstrap', 'C#', '.Net', 'MySQL', 'MongoDB'],
-    description: `Worked individually and in teams to build client side software using JavaScript, MVC, Vue.js, HTML 5, CSS 3, and Bootstrap. Created REST APIs using technologies including Node.js, Express, .net C#, MySQL, and MongoDB.
+    description: `Worked individually and in teams to build client side software using JavaScript, Vue.js, HTML, CSS, and Bootstrap. Created REST APIs using technologies including Node.js, Express, .net C#, MySQL, and MongoDB.
 
-    Created functional full-stack applications within teams and individually while implementing Scrum methodology.`
+    Created full-stack applications within teams and individually while implementing Scrum methodology.`
   }
 ]
 
@@ -78,6 +79,14 @@ const Experience = () => {
           setActiveSection={setActiveSection}
         />
       ))}
+      <div className="mt-2 ms-2">
+        <CustomLinkText
+          href={'https://caleb-elliott-assets.s3.us-west-2.amazonaws.com/Resume2023.pdf'}
+          text={'View Full CV'}
+          includeIcon={true}
+          spaceEnds={false}
+        />
+      </div>
     </div>
   )
 }
